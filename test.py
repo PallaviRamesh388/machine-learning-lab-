393f8d86ab7a14cb860db7cb6d3f318e9669eb58
PallaviRamesh388 <72143580+PallaviRamesh388@users.noreply.github.com>
2021-01-14 14:42:45 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,15 @@
+training data size=8
+test data size=6
+
+target   count   probability
+yes      4       0.5
+no       4       0.5
+
+instance prediction target
+1        no         yes
+2        yes        yes
+3        no         yes
+4        yes        yes
+5        yes        yes
+6        no         no
+accuracy 66.66666666666666%
